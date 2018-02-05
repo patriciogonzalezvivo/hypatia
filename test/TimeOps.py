42800896bdde37375f4ba3a3e972810bb4eeef80
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2018-02-05 15:52:38 -0800

    unit test for TimeOps

--- test/TimeOps.py
+++ test/TimeOps.py
@@ -13,38 +13,42 @@ import math
 from astro import *
 import ephem
 
-def test(jd):
+def testJDtoDatetime(jd, expeceted):
+  datetime = TimeOps.formatDateTime(jd, Y_M_D_HM)
+  return str("{:.1f}".format(jd)), datetime, ("[OK]" if datetime[0:len(datetime)-2] == expeceted[0:len(expeceted)-2] else  "[FAIL]")
+
+def testJDtoYMD(jd, expeceted):
   ymd = TimeOps.toYMD(jd)
   hms = TimeOps.toHMS(jd)
-  datetime = str(ymd[0])+'-'+"{:0>2d}".format(ymd[1])+'-'+"{:0>2d}".format(int(math.floor(ymd[2])))
-  datetime += " {:0>2d}".format(hms[0])+':'+"{:0>2d}".format(hms[1])
+  datetime = str(ymd[0])+'-'+"{:0>2d}".format(ymd[1])+'-'+"{:.1f}".format(ymd[2])
   datetime = str(datetime)
-  return str("{:.1f}".format(jd)), TimeOps.formatDateTime(jd, Y_M_D_HM), datetime
+  return str("{:.1f}".format(jd)), datetime, ("[OK]" if datetime == expeceted else  "[FAIL]")
 
-sec = TimeOps.getCurrentSeconds();
-print('Timestamp:\t\t\t\t\t', TimeOps.getCurrentSeconds())
-print('------------------------------------------------------------')
-
-e_date = datetime.datetime.utcnow()
-print('UTC:\t\t\t\t\t\t\t\t', e_date.strftime('%Y-%m-%d %H:%M:%S'))
-
-date = datetime.datetime.fromtimestamp(TimeOps.getCurrentSeconds());
-print('Local:\t\t\t\t\t\t\t', date.strftime('%Y-%m-%d %H:%M:%S'))
+now_jd = TimeOps.now()
+now_sec = TimeOps.getCurrentSeconds();
+now_date = datetime.datetime.utcnow()
+utc_date = now_date.strftime('%Y-%m-%d %H:%M')
 
 print('------------------------------------------------------------')
-print('Julian Date:\t\t\t\t', test((TimeOps.toJD(sec))) )
-print('Julian Date (ephem):', test(ephem.julian_date(e_date)) )
-print('Julian Date UTC:  \t', test(TimeOps.now()) )
-print('Julian Date Local:\t', test(TimeOps.now(True)) )
+print('Julian Date (UTC sec):\t', testJDtoDatetime(TimeOps.toJD(now_sec) + TimeOps.JD_DIFF, utc_date ) )
+print('Julian Date (ephem):\t\t', testJDtoDatetime(ephem.julian_date(now_date) + TimeOps.JD_DIFF, utc_date ) )
+print('Julian Date (UTC now()):', testJDtoDatetime(now_jd, utc_date ) )
 
 print('------------------------------------------------------------')
 
-print(test(2451545.0)) 
-print(test(2451179.5))
-print(test(2446822.5))
-print(test(1355866.5))
-print(test(1355671.4))
-print(test(0.0))
+print(testJDtoYMD(2451545.0, '2000-01-1.5')) 
+print(testJDtoYMD(2451179.5, '1999-01-1.0'))
+print(testJDtoYMD(2446822.5, '1987-01-27.0'))
+print(testJDtoYMD(2446966.0, '1987-06-19.5'))
+print(testJDtoYMD(2415020.5, '1900-01-1.0'))
+print(testJDtoYMD(2305447.5, '1600-01-1.0'))
+print(testJDtoYMD(2305812.5, '1600-12-31.0'))
+print(testJDtoYMD(2026871.8, '837-04-10.3'))
+print(testJDtoYMD(1676496.5, '-123-12-31.0'))
+print(testJDtoYMD(1676497.5, '-122-01-1.0'))
+print(testJDtoYMD(1356001.0, '-1000-07-12.5'))
+print(testJDtoYMD(1355866.5, '-1000-02-29.0'))
+print(testJDtoYMD(1355671.4, '-1001-08-17.9'))
+print(testJDtoYMD(0.0, '-4712-01-1.5'))
 
-print(TimeOps.getDOW(TimeOps.toDOW(TimeOps.toJD(sec)-.5)))
 
